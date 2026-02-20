66b3f7439c2df300ff0e7872b3372392eac6f07e
Maiksel <adityapatria27@gmail.com>
2026-02-20 07:37:42 +0700

    Stop tracking ROS 2 build artifacts and update gitignore

--- ros2_ws/build/py_pubsub/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ezra/Desktop/learning-week-subsriber-publisher/ros2_ws/install/py_pubsub'
--- ros2_ws/src/py_pubsub/setup.py
+++ ros2_ws/src/py_pubsub/setup.py
@@ -24,7 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-	   'talker = py_pubsub.publisher:main',
+	        'talker = py_pubsub.publisher:main',
             'listener = py_pubsub.subscriber:main',
         ],
     },
